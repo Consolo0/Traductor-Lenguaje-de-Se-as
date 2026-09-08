5c0e0de476837418f0caa6bcec5fc620a70f5102
Consolo0 <aconsolo@uc.cl>
2026-09-08 00:18:17 -0300

    fix: Change .bat for refacotring

--- main.bat
+++ main.bat
@@ -4,7 +4,7 @@ cd /d "%~dp0"
 
 set PYTHON_PATH="c:\Users\conso\AppData\Local\Programs\Python\Python312\python.exe"
 
-%PYTHON_PATH% models/realtime_infer.py --model "models/mlp_model.pt" --classes "models/mlp_model.classes.json" --min-detection-confidence 0.5 --min-model-confidence 0.6 --hold-seconds 0.8 --space-gap-seconds 1.5 --camera-index 0
+%PYTHON_PATH% models/realtime_infer.py --model "models/ModelInfo/mlp_model.pt" --classes "models/ModelInfo/mlp_model.classes.json" --min-detection-confidence 0.5 --min-model-confidence 0.6 --hold-seconds 0.8 --space-gap-seconds 1.5 --camera-index 0
 
 echo.
 echo Proceso finalizado.
